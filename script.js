55c306e5b53aacf75531e5b22c26a1c83915fd41
purwarsankalp4-stack <purwarsankalp4@gmail.com>
2026-04-17 16:45:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+body {
+  font-family: Arial;
+  text-align: center;
+}
 
+.board {
+  display: flex;
+  justify-content: space-around;
+}
+
+.column {
+  width: 30%;
+  background: #f4f4f4;
+  padding: 10px;
+  border-radius: 10px;
+}
+
+.task {
+  background: white;
+  margin: 5px;
+  padding: 10px;
+  border-radius: 5px;
+}
